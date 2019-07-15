f325c629f04563a3d6aaba1f8622d17f9352d584
dfzq <dfzq@MacBookPro.local>
2019-07-15 13:36:11 +0800

    add entry

--- types/fastman/index.d.ts
+++ types/fastman/index.d.ts
@@ -2,5 +2,49 @@
 // Project: http://itest.dfzq.com.cn/document/fastman-v2/index.html
 // Definitions by: crixusshen <https://github.com/crixusshen>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+import alert from 'fastman/alertman';
+import actionSheet from 'fastman/actionsheetman';
+import { blankPage } from 'fastman/blankpageman';
+import cascadePicker from 'fastman/cascadepickerman';
+import confirm from 'fastman/confirmman';
+import datetimepicker from 'fastman/datetimepickerman';
+import { upload } from 'fastman/fileuploadman';
+import { validate, validateExtend } from 'fastman/formvalidateman';
+import { showLoading, hideLoading } from 'fastman/loadingman';
+import { defaults, openModal, closeModal } from 'fastman/modalman';
+import passGuard from 'fastman/passguardman';
+import picker from 'fastman/pickerman';
+import { initInfiniteScroll, infiniteRefresh, infiniteScrollDone } from 'fastman/scrollman-infiniteRefresh';
+import { initScroller, refreshScroller, scrollTop } from 'fastman/scrollman';
+import { tabEvent, tabSwitch } from 'fastman/tabman';
+import { tip, closeTip } from 'fastman/tipman';
+import { toast, closeToast } from 'fastman/toastman';
 
-export function render(a: string): string;
+export {
+    alert,
+    actionSheet,
+    blankPage,
+    cascadePicker,
+    confirm,
+    datetimepicker,
+    upload,
+    validate,
+    validateExtend,
+    showLoading,
+    hideLoading,
+    defaults,
+    openModal,
+    closeModal,
+    passGuard,
+    picker,
+    initInfiniteScroll,
+    infiniteRefresh,
+    initScroller,
+    refreshScroller,
+    scrollTop,
+    tabEvent,
+    tabSwitch,
+    closeTip,
+    toast,
+    closeToast
+}
